cdcf590f0b38bfa189acf408a506a38e73b5ddae
Luis Loja <lloja@Luiss-iMac.local>
2017-03-26 19:29:27 +0100

    Initial Commit

---
+++ SwiftCmd/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftCmd
+//
+//  Created by Luis Loja on 26/03/2017.
+//  Copyright © 2017 Luis Loja. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
